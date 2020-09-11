7544ce98b11ee4541562529aa90e69ac268e846f
abenpy <dongshangshi@gmail.com>
2020-09-11 13:09:57 -0400

    3 maxpool

--- Pytorch-UNet-master/unet/unet_parts.py
+++ Pytorch-UNet-master/unet/unet_parts.py
@@ -31,7 +31,7 @@ class Down(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(3),
             DoubleConv(in_channels, out_channels)
         )
 
